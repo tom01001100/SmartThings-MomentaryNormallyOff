b8d2782a9ac64fc24c6bf568979bcb0b1abd94b7
tomlausman <48331511+tomlausman@users.noreply.github.com>
2020-12-08 14:38:27 -0700

    Update m00-MomentarySwitch-NormallyOff.groovy

--- src/m00-MomentarySwitch-NormallyOff.groovy
+++ src/m00-MomentarySwitch-NormallyOff.groovy
@@ -1,5 +1,5 @@
 metadata {
-	definition (name: "m00-MomentarySwitch-NormallyOn", namespace: "m00", author: "m00") {
+	definition (name: "m00-MomentarySwitch-NormallyOff", namespace: "m00", author: "m00") {
 		capability "Actuator"
 		capability "Switch"
 		capability "Momentary"
@@ -13,8 +13,9 @@ metadata {
 	// UI tile definitions
 	tiles {
 		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-			state "on", label: 'Push', action: "momentary.push", nextState: "off"
-			state "off", label: 'Push', action: "momentary.push"
+			state "off", label: 'Push', action: "momentary.push", nextState: "on"
+			state "on", label: 'Push', action: "momentary.push"
+			
 		}
 		main "switch"
 		details "switch"
@@ -25,8 +26,8 @@ def parse(String description) {
 }
 
 def push() {
-	sendEvent(name: "switch", value: "off", isStateChange: true, display: false)
 	sendEvent(name: "switch", value: "on", isStateChange: true, display: false)
+	sendEvent(name: "switch", value: "off", isStateChange: true, display: false)
 	sendEvent(name: "momentary", value: "pushed", isStateChange: true)
 }
 
